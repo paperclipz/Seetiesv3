580c7d5a75fd05f3f982687a60266f29aa1b1ef6
Evan Beh <imexlight@gmail.com>
2015-10-13 11:53:19 +0800

    profile page development

--- SeetiesIOS/profile/NewProfile/ProfileViewController.m
+++ SeetiesIOS/profile/NewProfile/ProfileViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ProfileViewController.h"
+#import "ProfileCollectionTableViewCell.h"
 
 @interface ProfileViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *ibTableView;
+@property (strong, nonatomic) IBOutlet UIView *ibTopContentView;
 
 @end
 
@@ -16,12 +19,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self initSelfView];
     // Do any additional setup after loading the view from its nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+-(void)initSelfView
+{
+
+
+}
+
+-(void)initTableViewWithDelegate:(id)delegate
+{
+    self.ibTableView.delegate = delegate;
+    self.ibTableView.dataSource = delegate;
+    [self.ibTableView registerClass:[ProfileCollectionTableViewCell class] forCellReuseIdentifier:@"ProfileCollectionTableViewCell"];
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    ProfileCollectionTableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"ProfileCollectionTableViewCell"];
+    
+    return cell;
 }
 
 /*
