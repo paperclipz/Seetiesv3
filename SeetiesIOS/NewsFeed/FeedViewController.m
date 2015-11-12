6eccec060dc53eba8f1cfb73cd375515038b9ff4
Evan Beh <imexlight@gmail.com>
2015-11-12 15:34:09 +0800

    arrange news feed code

--- SeetiesIOS/NewsFeed/FeedViewController.m
+++ SeetiesIOS/NewsFeed/FeedViewController.m
@@ -22,12 +22,133 @@
 #import "AnnounceViewController.h"
 #import "SuggestedCollectionsViewController.h"
 #import "OpenWebViewController.h"
+
 @interface FeedViewController ()
+{
+    //no connection view
+    IBOutlet UIView *NoConnectionView;
+    IBOutlet UILabel *ShowNoConnectionText;
+    IBOutlet UIImageView *NoConnectionImg;
+    IBOutlet UIButton *TryAgainButton;
+    IBOutlet UIScrollView *MainScroll;
+    IBOutlet UIScrollView *LocalScroll;
+    UIRefreshControl *refreshControl;
+    IBOutlet UIActivityIndicatorView *ShowActivity;
+    IBOutlet UILabel *ShowFeedText;
+    IBOutlet UIButton *SearchButton;
+    IBOutlet UIButton *FilterButton;
+    IBOutlet UIImageView *BarImage;
+    
+    int heightcheck;
+    int TestCheck;
+    int TotalCount;
+    int CheckInitData;
+    
+    NSMutableArray *arrAddress;
+    NSMutableArray *arrTitle;
+    NSMutableArray *arrMessage;
+    NSMutableArray *arrType;
+    NSMutableArray *arrImage;
+    NSMutableArray *arrUserImage;
+    NSMutableArray *arrUserName;
+    NSMutableArray *arrUserID;
+    NSMutableArray *arrDistance;
+    NSMutableArray *arrDisplayCountryName;
+    NSMutableArray *arrPostID;
+    NSMutableArray *arrImageWidth;
+    NSMutableArray *arrImageHeight;
+    NSMutableArray *arrlike;
+    NSMutableArray *arrCollect;
+    
+    NSMutableArray *User_IDArray;
+    NSMutableArray *User_ProfileImageArray;
+    NSMutableArray *User_NameArray;
+    NSMutableArray *User_LocationArray;
+    NSMutableArray *User_FollowArray;
+    NSMutableArray *User_UserNameArray;
+    NSMutableArray *User_PhotoArray;
+    
+    NSMutableArray *arrType_Announcement;
+    NSMutableArray *arrID_Announcement;
+    
+    NSMutableArray *arrAboadID;
+    NSMutableArray *arrfeaturedUserName;
+    NSMutableArray *arrFriendUserName;
+    NSMutableArray *arrDealID;
+    
+    NSMutableArray *arrCollectionID;
+    NSMutableArray *arrCollectionName;
+    NSMutableArray *arrCollectionDescription;
+    NSMutableArray *arrCollectionFollowing;
+    NSMutableArray *arrCollectionUserID;
+    
+    
+    NSString *GetNextPaging;
+    
+    
+    NSDate *methodStart;
+    
+    IBOutlet UILabel *ShowUpdateText;
+    
+    UIScrollView *SuggestedScrollview_Deal;
+    UIPageControl *SuggestedpageControl_Deal;
+    UILabel *ShowSuggestedCount_Deal;
+    
+    UIScrollView *SuggestedScrollview_Aboad;
+    UIPageControl *SuggestedpageControl_Aboad;
+    UILabel *ShowSuggestedCount_Aboad;
+    
+    UIScrollView *SUserScrollview_Friend;
+    UIPageControl *SUserpageControl_Friend;
+    UILabel *ShowSUserCount_Friend;
+    
+    UIScrollView *SUserScrollview_Featured;
+    UIPageControl *SUserpageControl_Featured;
+    UILabel *ShowSUserCount_Featured;
+    
+    UIScrollView *CollectionScrollview;
+    
+    UrlDataClass *DataUrl;
+    
+    NSURLConnection *theConnection_All;
+    NSURLConnection *theConnection_likes;
+    NSURLConnection *theConnection_QuickCollect;
+    NSURLConnection *theConnection_TrackPromotedUserViews;
+    
+    NSString *latPoint;
+    NSString *lonPoint;
+    NSString *ExternalIPAddress;
+    
+    NSMutableData *webData;
+    
+    // NSInteger TotalPage;
+    // NSInteger CurrentPage;
+    NSInteger DataCount;
+    NSInteger Offset;
+    
+    int CheckFirstTimeLoad;
+    BOOL OnLoad;
+    
+    //send like data
+    NSString *SendLikePostID;
+    NSString *CheckLike;
+    NSString *CheckCollect;
+    NSString *GetPostID;
+    
+    UIButton *MainNearbyButton;
+    
+    //tracker url
+    NSString *TrackerUrl;
+    
+    UIView *RateView;
+}
 @property (strong, nonatomic) CLLocationManager *locationManager;
 @property (strong, nonatomic) CLLocation *location;
 @end
 
 @implementation FeedViewController
+- (IBAction)btnTestClicked:(id)sender {
+}
 #define IS_OS_8_OR_LATER ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
 - (void)viewDidLoad {
     [super viewDidLoad];
