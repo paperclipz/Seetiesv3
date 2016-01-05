0f1cd74f7118ff2776ba0d3f916f0e4b2e30d025
Evan Beh <imexlight@gmail.com>
2016-01-05 18:27:42 +0800

    add wallet listing

---
+++ SeetiesIOS/SeetiesV3/ListingV3/WalletListing/WalletListingViewController.h
@@ -0,0 +1,13 @@
+//
+//  WalletListingViewController.h
+//  SeetiesIOS
+//
+//  Created by Evan Beh on 1/5/16.
+//  Copyright © 2016 Stylar Network. All rights reserved.
+//
+
+#import "CommonViewController.h"
+
+@interface WalletListingViewController : CommonViewController
+
+@end
