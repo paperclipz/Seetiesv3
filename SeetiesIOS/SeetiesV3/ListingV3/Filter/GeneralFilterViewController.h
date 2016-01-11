9387a0a6ab51f6ef947dea6eae599c7daec69a41
Lup Meng Poo <lupmeng@seeties.me>
2016-01-11 19:13:10 +0800

    adding general filter

---
+++ SeetiesIOS/SeetiesV3/ListingV3/Filter/GeneralFilterViewController.h
@@ -0,0 +1,13 @@
+//
+//  GeneralFilterViewController.h
+//  SeetiesIOS
+//
+//  Created by Lup Meng Poo on 11/01/2016.
+//  Copyright © 2016 Stylar Network. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GeneralFilterViewController : UIViewController
+
+@end
