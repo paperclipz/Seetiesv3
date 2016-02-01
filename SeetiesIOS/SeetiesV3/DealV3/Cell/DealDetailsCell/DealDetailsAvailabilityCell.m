9fb64b620d4150e4fad08b83467bfce192f37769
Lup Meng Poo <lupmeng@seeties.me>
2016-02-01 19:37:37 +0800

    adding Deal Details page

---
+++ SeetiesIOS/SeetiesV3/DealV3/Cell/DealDetailsCell/DealDetailsAvailabilityCell.m
@@ -0,0 +1,23 @@
+//
+//  DealDetailsAvailabilityCell.m
+//  SeetiesIOS
+//
+//  Created by Lup Meng Poo on 29/01/2016.
+//  Copyright © 2016 Stylar Network. All rights reserved.
+//
+
+#import "DealDetailsAvailabilityCell.h"
+
+@implementation DealDetailsAvailabilityCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ SeetiesIOS/SeetiesV3/DealV3/Cell/DealDetailsCell/NearbyShopsCell.h
@@ -0,0 +1,13 @@
+//
+//  NearbyShopsCell.h
+//  SeetiesIOS
+//
+//  Created by Lup Meng Poo on 01/02/2016.
+//  Copyright © 2016 Stylar Network. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NearbyShopsCell : CommonCollectionViewCell
+
+@end
