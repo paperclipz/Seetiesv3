89e434b6c8abde67c7a32006b7390faf212e6a21
Lup Meng Poo <lupmeng@seeties.me>
2016-03-30 18:56:40 +0800

    added share deals

--- SeetiesIOS/SeetiesV3/DealV3/DealDetailsViewController.m
+++ SeetiesIOS/SeetiesV3/DealV3/DealDetailsViewController.m
@@ -112,6 +112,7 @@
 @property(nonatomic) TermsViewController *termsViewController;
 @property(nonatomic) ReportProblemViewController *reportProblemViewController;
 @property(nonatomic) PhotoViewController *photoViewController;
+@property(nonatomic) ShareV2ViewController *shareViewController;
 
 @end
 
@@ -412,6 +413,13 @@
         _reportProblemViewController = [ReportProblemViewController new];
     }
     return _reportProblemViewController;
+}
+
+-(ShareV2ViewController *)shareViewController{
+    if (!_shareViewController) {
+        _shareViewController = [ShareV2ViewController new];
+    }
+    return _shareViewController;
 }
 
 #pragma mark - UpdateView
@@ -850,6 +858,18 @@
 
 #pragma mark - Delegate
 - (IBAction)buttonShareClicked:(id)sender {
+    self.shareViewController = nil;
+    UINavigationController* naviVC = [[UINavigationController alloc]initWithRootViewController:self.shareViewController];
+    [naviVC setNavigationBarHidden:YES animated:NO];
+    PhotoModel *photo = self.dealModel.photos[0];
+    NSString *shareTitle = [Utils isStringNull:self.dealModel.title]? self.dealModel.cover_title : self.dealModel.title;
+    [self.shareViewController share:@"" title:shareTitle imagURL:photo.imageURL shareType:ShareTypeDeal shareID:self.dealModel.dID userID:[Utils getUserID]];
+    
+    MZFormSheetPresentationViewController *formSheetController = [[MZFormSheetPresentationViewController alloc] initWithContentViewController:naviVC];
+    formSheetController.presentationController.contentViewSize = [Utils getDeviceScreenSize].size;
+    formSheetController.presentationController.shouldDismissOnBackgroundViewTap = YES;
+    formSheetController.contentViewControllerTransitionStyle = MZFormSheetPresentationTransitionStyleSlideFromBottom;
+    [self presentViewController:formSheetController animated:YES completion:nil];
 }
 
 - (IBAction)buttonTranslateClicked:(id)sender {
