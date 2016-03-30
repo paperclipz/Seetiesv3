43ada410ed8346fa1d3992c21b45735640000327
Lup Meng Poo <lupmeng@seeties.me>
2016-03-30 11:25:25 +0800

    updating deal details term&conditions

--- SeetiesIOS/SeetiesV3/DealV3/DealDetailsViewController.m
+++ SeetiesIOS/SeetiesV3/DealV3/DealDetailsViewController.m
@@ -670,7 +670,7 @@
                 break;
             }
             
-            UILabel *bulletPoint = [[UILabel alloc] initWithFrame:CGRectMake(0, yOrigin+3, 7, 7)];
+            UILabel *bulletPoint = [[UILabel alloc] initWithFrame:CGRectMake(0, yOrigin+3, 16, 7)];
             bulletPoint.font = [UIFont systemFontOfSize:fontSize];
             bulletPoint.textColor = [UIColor colorWithRed:204/255.0f green:204/255.0f blue:204/255.0f alpha:1];
             bulletPoint.text = [NSString stringWithFormat:@"\u2022"];
@@ -679,8 +679,8 @@
             if (count == 4) {
                 formattedTerm = [NSString stringWithFormat:@"%@ ...", term];
             }
-            CGRect rect = [formattedTerm boundingRectWithSize:CGSizeMake(self.ibTnCContent.frame.size.width-16, CGFLOAT_MAX) options:NSStringDrawingUsesLineFragmentOrigin attributes:attr context:nil];
-            UILabel *tncLbl = [[UILabel alloc]initWithFrame:CGRectMake(16, yOrigin, ceilf(rect.size.width), ceilf(rect.size.height))];
+            CGRect rect = [formattedTerm boundingRectWithSize:CGSizeMake(self.ibTnCContent.frame.size.width-bulletPoint.frame.size.width, CGFLOAT_MAX) options:NSStringDrawingUsesLineFragmentOrigin attributes:attr context:nil];
+            UILabel *tncLbl = [[UILabel alloc]initWithFrame:CGRectMake(bulletPoint.frame.size.width, yOrigin, ceilf(rect.size.width), ceilf(rect.size.height))];
             tncLbl.font = [UIFont systemFontOfSize:fontSize];
             tncLbl.textColor = [UIColor colorWithRed:153/255.0f green:153/255.0f blue:153/255.0f alpha:1];
             tncLbl.numberOfLines = 0;
