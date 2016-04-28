3096c64d5249e213270bfb0f8c54d868cc943d62
Evan Beh <imexlight@gmail.com>
2016-04-28 11:57:46 +0800

    1. commit partial development and invite friend

---
+++ SeetiesIOS/SeetiesV3/InviteFriendV3/CT3_InviteFriendViewController.h
@@ -0,0 +1,13 @@
+//
+//  CT3_InviteFriendViewController.h
+//  Seeties
+//
+//  Created by Evan Beh on 28/04/2016.
+//  Copyright © 2016 Stylar Network. All rights reserved.
+//
+
+#import "CommonViewController.h"
+
+@interface CT3_InviteFriendViewController : CommonViewController
+
+@end
---
+++ SeetiesIOS/SeetiesV3/InviteFriendV3/CT3_InviteFriendViewController.m
@@ -0,0 +1,37 @@
+//
+//  CT3_InviteFriendViewController.m
+//  Seeties
+//
+//  Created by Evan Beh on 28/04/2016.
+//  Copyright © 2016 Stylar Network. All rights reserved.
+//
+
+#import "CT3_InviteFriendViewController.h"
+
+@interface CT3_InviteFriendViewController ()
+
+@end
+
+@implementation CT3_InviteFriendViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
