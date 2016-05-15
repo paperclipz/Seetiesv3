c17d31d766eca5502302ad6025a1353bf44e4669
Evan Beh <imexlight@gmail.com>
2016-05-15 16:36:24 +0800

    1. [fix] Chinese Font type for search

--- SeetiesIOS/SeetiesV3/ShareV3/SeetiesIOS-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import "CommonCollectionViewCell.h"
-#import "ShareToFrenViewController.h"
-#import "ShareManager.h"
-#import "Utils.h"
-#import "LanguageManager.h"
